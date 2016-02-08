3466f4689cf9ba29a58e14c1e19d60d0e4a0ad63
hmaeck <hendrik.maeckelberghe@student.ucll.be>
2016-02-08 13:19:49 +0100

    init project

---
+++ src/main/java/be/projecttycoon/ProjecttycoonApplication.java
@@ -0,0 +1,12 @@
+package be.projecttycoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttycoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttycoonApplication.class, args);
+	}
+}
---
+++ src/test/java/be/projecttycoon/ProjecttycoonApplicationTests.java
@@ -0,0 +1,18 @@
+package be.projecttycoon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ProjecttycoonApplication.class)
+@WebAppConfiguration
+public class ProjecttycoonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
