18eeaa7aab658cdef5cb0914a4396ee52bbd2abc
thomas_g <thomasgeboers@gmail.com>
2016-02-08 14:51:20 +0100

    simple index page

--- src/main/java/be/projecttycoon/ProjecttycoonApplication.java
+++ src/main/java/be/projecttycoon/ProjecttycoonApplication.java
@@ -2,8 +2,10 @@ package be.projecttycoon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class ProjecttycoonApplication {
 
 	public static void main(String[] args) {
