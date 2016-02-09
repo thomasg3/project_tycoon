e07f01c155da6cc9f04efc82f36a70cb79ad06ff
thomas_g <thomasgeboers@gmail.com>
2016-02-09 08:54:26 +0100

    Change in memory users

--- src/main/java/be/projecttycoon/auth/AuthConfig.java
+++ src/main/java/be/projecttycoon/auth/AuthConfig.java
@@ -3,6 +3,7 @@ package be.projecttycoon.auth;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
@@ -37,4 +38,12 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
         return repository;
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("password").roles("USER", "ADMIN");
+    }
 }
