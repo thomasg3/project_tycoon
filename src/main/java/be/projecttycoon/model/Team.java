e4e665317921eb92f4025f8f59e0c05d897e46c4
newbould5 <michael_newbould@hotmail.com>
2016-02-09 13:44:34 +0100

    deleted rubbish

--- src/main/java/be/projecttycoon/model/Team.java
+++ src/main/java/be/projecttycoon/model/Team.java
@@ -7,31 +7,34 @@ import javax.validation.constraints.NotNull;
 /**
  * Created by thomas on 08/02/16.
  */
-public class Team {
 
-    private int id;
-    private String teamName,password;
+public class Team extends User{
+
+
     private String teamImage;
     private int score, likes;
     private boolean registered;
 
-    //todo score, likes - errors/bean validation
+    //todo errors/bean validation
 
     //constructors
-    public Team(){}
+    public Team(){
+        super();
+    }
 
     public Team(int id,String teamName, String password, String path){
-        setId(id);
-        setTeamName(teamName);
-        setPassword(password);
+        super(password,teamName);
         setTeamImage(path);
         setRegistered(false);
     }
 
 
+    public void setScore(int score) {
+        this.score = score;
+    }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setLikes(int likes) {
+        this.likes = likes;
     }
 
     public void setTeamImage(String path){
@@ -39,28 +42,11 @@ public class Team {
         //this.teamImage = ImageIO.read(new File(path));
         this.teamImage=path;
     }
-    public void setTeamName(@NotNull String teamName) {
-        this.teamName = teamName;
-    }
-    public void setPassword(@NotNull String password) {
-        this.password = password;
-    }
 
     public void setRegistered(boolean registered){
         this.registered=registered;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getTeamName() {
-        return teamName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 
     public String getTeamImage() {
         return teamImage;
@@ -69,7 +55,15 @@ public class Team {
         return this.registered;
     }
 
-    public void register(String password,String path){
+    public int getScore() {
+        return score;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void register(String password, String path){
         setPassword(password);
         setTeamImage(path);
         setRegistered(true);
