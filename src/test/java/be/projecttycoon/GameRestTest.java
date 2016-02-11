8dd796e50d6e3e8b7ff2580ecb286ca9e273010c
hmaeck <hendrik.maeckelberghe@student.ucll.be>
2016-02-11 17:06:02 +0100

    Game Testing

--- src/test/java/be/projecttycoon/GameRestTest.java
+++ src/test/java/be/projecttycoon/GameRestTest.java
@@ -2,11 +2,18 @@ package be.projecttycoon;
 
 import be.projecttycoon.db.GameRepository;
 import be.projecttycoon.model.Game;
+import be.projecttycoon.model.Team;
+import be.projecttycoon.rest.GameResource;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.specification.RequestSpecification;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -15,68 +22,106 @@ import static org.hamcrest.Matchers.equalTo;
 /**
  * Created by kiwi on 11/02/2016.
  */
-public class GameRestTest {
 
-    private RequestSpecification adminRequestSpecification;
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ProjecttycoonApplication.class)
+@WebAppConfiguration
+public class GameRestTest extends SuperTest{
 
     @Autowired
     private GameRepository gameRepository;
 
 
-    @Before
-    public void initialize() {
-        RestAssured.baseURI = "https://localhost";
-        RestAssured.basePath = "/api/games";
-        RestAssured.port = 8443;
-        RestAssured.useRelaxedHTTPSValidation();
-        adminRequestSpecification = createAdminRequestSpecification();
+
+    public GameRepository getGameRepository(){
+        return gameRepository;
     }
 
-    private RequestSpecification createAdminRequestSpecification(){
-        //Registered user!!!
-        return given().auth().basic("jef", "jef").contentType("application/json");
+    public void setGameRepository(GameRepository gameRepository){
+        this.gameRepository=gameRepository;
     }
+
+    public GameRestTest() {
+        RestAssured.basePath = "/api/games";
+    }
+
 
 
     @Test
     public void getAllGamesWhenRegistered(){
-        adminRequestSpecification.expect().statusCode(200).when().get();
+        AuthorizedRegisteredRequestSpecification.expect().statusCode(ALLOWED).when().get();
     }
 
     @Test
     public void getAllGamesWhenAuthenticatedButNotRegisteredShouldBeBlocked(){
-        given().auth().basic("jos", "jos").expect().statusCode(403).when().get();
+        UnregisteredRequestSpecification.expect().statusCode(FORBIDDEN).when().get();
     }
 
     @Test
     public void getAllGamesWhenNotAuthenticatedShouldBeBlocked(){
-        given().auth().basic("username", "wrongpassword").expect().statusCode(401).when().get();
+        UnauthorizedRequestSpecification.expect().statusCode(UNAUTHORIZED).when().get();
     }
 
     @Test
     public void getGameByIdWhenRegistered(){
-        adminRequestSpecification.expect().statusCode(200).when().get("/1");
+        AuthorizedRegisteredRequestSpecification.expect().statusCode(ALLOWED).when().get("/1");
     }
+
 
     @Test
     public void getGameByIdWhenAuthenticatedButNotRegisteredShouldBeBlocked(){
-        adminRequestSpecification.expect().statusCode(401).when().get("/1");
+        UnregisteredRequestSpecification.expect().statusCode(FORBIDDEN).when().get("/1");
     }
 
     @Test
     public void getGameByIdWhenNotAuthenticatedShouldBeBlocked(){
-        given().auth().basic("username", "wrongpassword").expect().statusCode(401).when().get("/1");
+        UnauthorizedRequestSpecification.expect().statusCode(UNAUTHORIZED).when().get("/1");
     }
 
 
     @Test
-    public void createGameAndGetItWhileUnregisteredButAuthenticatedShouldBeBlocked(){
+    public void createGameAndGetItWhileUnauthenticatedShouldBeBlocked(){
         Game game = new Game("A game",4);
         game = gameRepository.save(game);
-        adminRequestSpecification.expect().statusCode(401).when().get("/"+game.getId());
-
-
+        UnauthorizedRequestSpecification.expect().statusCode(UNAUTHORIZED).when().get("/"+game.getId());
 
     }
 
+    @Test
+    public void createGameAndGetItWhileUnregisteredShouldBeBlocked(){
+        Game game = new Game("AnotherGame",5);
+        game = gameRepository.save(game);
+        UnregisteredRequestSpecification.expect().statusCode(FORBIDDEN).when().get("/"+game.getId());
+    }
+
+    @Test
+    public void createGameAndGetItWhileRegisteredShouldBeBlocked(){
+        Game game = new Game("YetAnotherGame",5);
+        game = gameRepository.save(game);
+        AuthorizedRegisteredRequestSpecification.expect().statusCode(ALLOWED).when().get("/"+game.getId());
+    }
+
+
+
+    //Bypassing REST api via GameRepo isn't a good idea. And gives misleading test results.
+    @Test
+    public void createGameAndCheckIfUsersAreInGame(){
+        Game game = new Game("Final Game",3);
+        game = getGameRepository().save(game);
+        System.out.println("The id of the game is:" + game.getId());
+        System.out.println("Het aantal teams is: " +game.getTeams().size());
+
+
+        for(Team team:game.getTeams()){
+            System.out.println("The teams are: "+ team.getTeamname());
+        }
+
+        for(Team team:game.getTeams()){
+            AuthorizedRegisteredRequestSpecification.expect().when().
+                    get("/game/"+team.getTeamname()).
+                    then()
+
+                    .statusCode(ALLOWED);
+        }
+    }
 }
--- src/test/java/be/projecttycoon/TeamRestTest.java
+++ src/test/java/be/projecttycoon/TeamRestTest.java
@@ -1,5 +1,10 @@
 package be.projecttycoon;
 
+import be.projecttycoon.controller.TestController;
+import be.projecttycoon.db.TeamRepository;
+import be.projecttycoon.model.Team;
+import be.projecttycoon.rest.GameResource;
+import be.projecttycoon.rest.TeamResource;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.authentication.FormAuthConfig;
 import com.jayway.restassured.response.Cookie;
@@ -8,6 +13,11 @@ import com.jayway.restassured.response.Headers;
 import com.jayway.restassured.specification.RequestSpecification;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -17,54 +27,43 @@ import static org.hamcrest.Matchers.*;
 /**
  * Created by kiwi on 11/02/2016.
  */
-public class TeamRestTest {
-    private RequestSpecification adminRequestSpecification;
 
-    @Before
-    public void initialize() {
-        RestAssured.baseURI = "https://localhost";
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ProjecttycoonApplication.class)
+@WebAppConfiguration
+public class TeamRestTest extends SuperTest{
+    @Autowired
+    private TeamRepository teamRepository;
+
+
+    public TeamRepository getTeamRepository(){
+        return this.teamRepository;
+    }
+
+    public void setTeamRepository(TeamRepository teamRepository){
+        this.teamRepository = teamRepository;
+    }
+
+
+    public TeamRestTest() {
         RestAssured.basePath = "/api/teams";
-        RestAssured.port = 8443;
-        RestAssured.useRelaxedHTTPSValidation();
-        adminRequestSpecification = createAdminRequestSpecification();
-    }
-
-    private Cookie getXsrfCookie(){
-        Headers xcsrf = adminRequestSpecification.expect().when().get("/search/jos").getHeaders();
-        String newToken=null;
-        for(Header hdr:xcsrf){
-            System.out.println(hdr);
-            if(hdr.getValue().contains("XSRF-TOKEN")){
-                String[] values= hdr.getValue().split("Set-Cookie=");
-                String[] splitted = values[0].split("XSRF-TOKEN=");
-                newToken = splitted[1].substring(0, splitted[1].length());
-                break;
-            }
-        }
-        System.out.println("The extracted token is: " + newToken);
-        return new Cookie.Builder("XSRF-TOKEN",newToken).build();
-    }
-
-
-    private RequestSpecification createAdminRequestSpecification(){
-        return given().auth().basic("jos", "jos").contentType("application/json");
     }
 
 
     @Test
     public void viewTeamsWhenAuthenticatedAsInvalidUserShouldBeBlocked(){
-        given().auth().basic("username", "wrongpassword").expect().statusCode(401).when().get();
+        UnauthorizedRequestSpecification.expect().statusCode(UNAUTHORIZED).when().get();
     }
 
     @Test
     public void viewTeamsWhenAuthenticatedAsValidUser(){
-        adminRequestSpecification.expect().statusCode(200).when().get();
+        AuthorizedRegisteredRequestSpecification.expect().statusCode(ALLOWED).when().get();
     }
 
 
     @Test
     public void searchForUserWithIdSixWhenAuthenticated(){
-        adminRequestSpecification.expect().when().
+        AuthorizedRegisteredRequestSpecification.expect().when().
                 get("/6").
                 then().
                 body(containsString("id")).
@@ -80,12 +79,12 @@ public class TeamRestTest {
 
     @Test
     public void searchForUserWithIdSixWhenNotAuthenticatedShouldBeBlocked(){
-        given().auth().basic("username", "wrongpassword").expect().statusCode(401).when().get("/6");
+        UnauthorizedRequestSpecification.expect().statusCode(UNAUTHORIZED).when().get("/6");
     }
 
     @Test
     public void searchForJosWhenAuthenticated(){
-        adminRequestSpecification.expect().when().
+        AuthorizedRegisteredRequestSpecification.expect().when().
                 get("/search/jos").
                 then().
                 body(containsString("id")).
@@ -95,78 +94,48 @@ public class TeamRestTest {
                 body(containsString("registered")).
                 body(containsString("admin")).
                 body("teamname", equalTo("jos"))
-                .statusCode(200);
+                .statusCode(ALLOWED);
     }
 
 
     @Test
     public void searchForJosWhenNotAuthenticatedShouldBeBlocked(){
-        given().auth().basic("username", "wrongpassword").expect().statusCode(401).when().get("/search/jos");
+        UnauthorizedRequestSpecification.expect().statusCode(UNAUTHORIZED).when().get("/search/jos");
     }
 
 
     @Test
-    public void checkThatJosIsNotRegisteredWhenAuthenticated(){
-        adminRequestSpecification.expect().when().
+    public void checkThatJosIsNotRegisteredWhenAuthenticatedAndRegister(){
+        UnregisteredRequestSpecification.expect().when().
                 get("/search/jos/registered").
                 then().
-                body("registered", equalTo(false)).statusCode(200);
+                body("registered", equalTo(false)).statusCode(ALLOWED);
+
+
+        Team team = getTeamRepository().findByTeamname("jos");
+        team.register("jos", "jos", "");
+        getTeamRepository().save(team);
+
+
+
+        //He isn't unregistered anymore... /giphy Mind Blown!
+        //Note: dit kan alle testen stuk makenen gezien onze unregistered user nu registered is...
+        UnregisteredRequestSpecification.expect().when().
+                get("/search/jos/registered").
+                then().
+                body("registered", equalTo(false)).statusCode(ALLOWED);
+    }
+
+    @Test
+    public void checkThatJefIsRegisteredWhenAuthenticated(){
+        AuthorizedRegisteredRequestSpecification.expect().when().
+                get("/search/jef/registered").
+                then().
+                body("registered", equalTo(true)).statusCode(ALLOWED);
     }
 
     @Test
     public void checkThatJosIsNotRegisteredWhenNotAuthenticatedShouldBeBlocked(){
-        given().auth().basic("username", "wrongpassword").expect().statusCode(401).when().get("/search/jos/registered");
+        UnauthorizedRequestSpecification.expect().statusCode(UNAUTHORIZED).when().get("/search/jos/registered");
     }
-
-
-
-
-
-
-    //Werkt niet via xsrf
-   /* @Test
-    public void checkRegisteringForJos(){
-
-        //By default Jos is unregistered
-        adminRequestSpecification.expect().when().
-                get("/search/jos").
-                then().
-                body(containsString("id")).
-                body(containsString("teamname")).
-                body(containsString("score")).
-                body(containsString("likes")).
-                body(containsString("registered")).
-                body(containsString("admin")).
-                body("registered", equalTo(false))
-                .statusCode(200);
-
-
-         Cookie cookie =  getXsrfCookie()
-        //When Jos Updates, registered is set to true
-        adminRequestSpecification.cookie(cookie).
-                 body("{\"oldUsername\":\"jos\",\"oldPassword\":\"jos\",\"newUsername\":\"jos\",\"newPassword\":\"jos\"}")
-                .when()
-                .put("/1");*/
-
-        // << "HTTP/1.1 403 Forbidden[\r][\n]" ?????????
-
-
-
-
-       /* adminRequestSpecification.expect().when().
-                get("/search/jos").then().
-                body(containsString("id")).
-                body(containsString("teamname")).
-                body(containsString("score")).
-                body(containsString("likes")).
-                body(containsString("registered")).
-                body(containsString("admin")).
-                body("registered", equalTo(true))
-                .statusCode(200);*/
-    //}
-
-
-
-
-
 }
