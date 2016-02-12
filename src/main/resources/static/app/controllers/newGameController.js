5e90d08a1f85d7d56fb5a81c70e388964121b87d
newbould5 <michael_newbould@hotmail.com>
2016-02-12 14:24:25 +0100

    merged

--- src/main/resources/static/app/controllers/newGameController.js
+++ src/main/resources/static/app/controllers/newGameController.js
@@ -5,9 +5,12 @@ angular.module('projecttycoonControllers')
     .controller('newGameController', function($scope, $location ,KnowledgeAreaResource, GameResource){
         $scope.knowledgeareas = KnowledgeAreaResource.query();
         $scope.game = new GameResource();
+        $scope.freeze = false;
         $scope.submit = function(){
+            $scope.freeze = true;
             $scope.game.$save(function(data){
                 $location.path('/dashboard/'+data.id).replace();
-            });
+
+            }, function(err){$scope.freeze = false;});
         };
     });
