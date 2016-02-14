ce445ae98dc3a21de6afb042f63a1ceb2e63017c
jeroenpinxten <jeroen.pinxten@gmail.com>
2016-02-14 01:24:25 +0100

    registerTeam client side validation + password repeat

--- src/main/resources/static/app/Validators.js
+++ src/main/resources/static/app/Validators.js
@@ -1,29 +1,7 @@
 /**
  * Created by Jeroen on 12-2-2016.
  */
-//var GAMENAME_REGEXP = /^[A-Za-z0-9\s]*$/;
 
-function MinLengthValidator(){
-    return {
-        restrict: 'A',
-        require: 'ngModel',
-        link: function($scope, $elm, $attrs, ctrl) {
-            $scope.$watch($attrs.ngModel, function(data) {
-                if(ctrl.$dirty){
-                    if($attrs.min){
-                        if(data.length  >= $attrs.min){
-                            ctrl.$setValidity($attrs.name + "length", true);
-                            ctrl.$valid = true;
-                        }else{
-                            ctrl.$setValidity($attrs.name + "length", false);
-                            ctrl.$valid = false;
-                        }
-                    }
-                }
-            });
-        }
-    };
-}
 
 function RegexValidator(){
     return {
@@ -31,15 +9,13 @@ function RegexValidator(){
         require: 'ngModel',
         link: function($scope, $elm, $attrs, ctrl) {
             $scope.$watch($attrs.ngModel, function(data) {
-                if(ctrl.$dirty){
+                if(ctrl.$dirty || ctrl.$touched){
                     if($attrs.regex && data){
                         var regex = new RegExp($attrs.regex);
                         if(regex.test(data)){
                             ctrl.$setValidity($attrs.name + "validregex", true);
-                            ctrl.$valid = true;
                         }else{
                             ctrl.$setValidity($attrs.name + "validregex", false);
-                            ctrl.$valid = false;
                         }
                     }
                 }
@@ -48,6 +24,24 @@ function RegexValidator(){
     };
 }
 
-angular.module('projecttycoon').directive('minLengthValidator', MinLengthValidator);
+function PasswordConfirm() {
+    return {
+        restrict: 'A',
+        require: "ngModel",
+        link: function ($scope, $element, $attrs, ctrl) {
+            $scope.$watch(function () {
+                if(ctrl.$dirty || ctrl.$touched) {
+                    if ($attrs.password === ctrl.$viewValue) {
+                        ctrl.$setValidity($attrs.name + "confirmPassword", true);
+                    } else {
+                        ctrl.$setValidity($attrs.name + "confirmPassword", false);
+                    }
+                }
+            });
+        }
+    };
+}
+
+angular.module('projecttycoon').directive('passwordConfirm', PasswordConfirm);
 angular.module('projecttycoon').directive('regexValidator', RegexValidator);
 
