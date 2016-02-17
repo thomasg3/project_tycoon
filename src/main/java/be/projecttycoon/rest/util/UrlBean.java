16781a653c8fc2cd5b3f216ee2e0d8fa6cafeefe
hmaeck <hendrik.maeckelberghe@student.ucll.be>
2016-02-17 09:24:10 +0100

    urlbean

---
+++ src/main/java/be/projecttycoon/rest/util/UrlBean.java
@@ -0,0 +1,22 @@
+package be.projecttycoon.rest.util;
+
+/**
+ * Created by kiwi on 17/02/2016.
+ */
+public class UrlBean {
+    private String url;
+
+
+    public UrlBean(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
+
