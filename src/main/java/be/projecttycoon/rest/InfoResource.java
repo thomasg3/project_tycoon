1f49251e31eb1e4eb1ec7f661877dd2cefdb47a3
newbould5 <michael_newbould@hotmail.com>
2016-02-18 14:57:48 +0100

    get all info untill current level

--- src/main/java/be/projecttycoon/rest/InfoResource.java
+++ src/main/java/be/projecttycoon/rest/InfoResource.java
@@ -1,8 +1,13 @@
 package be.projecttycoon.rest;
 
+import be.projecttycoon.db.GameRepository;
 import be.projecttycoon.db.InfoRepository;
+import be.projecttycoon.model.Game;
 import be.projecttycoon.model.Info;
 import be.projecttycoon.model.InfoType;
+import be.projecttycoon.model.level.Level;
+import be.projecttycoon.model.level.LevelState;
+import be.projecttycoon.model.level.Open;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by michael on 18/02/16.
@@ -25,13 +32,15 @@ public class InfoResource {
 //game isnt finished yet so i cant do this yet
 
     private final InfoRepository infoRepository;
+    private final GameRepository gameRepository;
 
     @Autowired
-    public InfoResource(InfoRepository infoRepository){
+    public InfoResource(InfoRepository infoRepository, GameRepository gameRepository){
         this.infoRepository=infoRepository;
-        Info i = new Info(1,"test info", "http://i.imgur.com/wmZppFn.jpg", InfoType.Image);
+        this.gameRepository=gameRepository;
+        Info i = new Info(1,"test info", "http://i.imgur.com/1rHMtFM.gif", InfoType.Image);
         Info i2 = new Info(1,"test video","https://www.youtube.com/embed/czezOcHfLS4",InfoType.Video);
-        Info i3 = new Info(1,"test document","/documents/changemanagement.pdf",InfoType.Document);
+        Info i3 = new Info(2,"test document","/documents/changemanagement.pdf",InfoType.Document);
         infoRepository.save(i);
         infoRepository.save(i2);
         infoRepository.save(i3);
@@ -45,8 +54,16 @@ public class InfoResource {
 
     @RequestMapping(value="/{id}",method = RequestMethod.GET)
     @Produces("application/json")
-    public Info getInfo(@PathVariable long id){
-        return infoRepository.findOne(id);
+    public Collection<Info> getInfo(@PathVariable long id){
+        Game g = gameRepository.findOne(id);
+       int round = g.getLevels().stream()
+                .filter(l->l.getState().equals("Open"))
+                .mapToInt(Level::getRound)
+                .max().orElse(-1);
+        Collection<Info> info = infoRepository.findAll().stream()
+                .filter(i->i.getUnlockedAtLevel() <= round)
+                .collect(Collectors.toList());
+        return info;
     }
 
 
