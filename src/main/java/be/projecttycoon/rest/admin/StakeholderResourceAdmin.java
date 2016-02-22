24ffac9ba9cad060e5995a7abe7c4c68295a833a
hmaeck <hendrik.maeckelberghe@student.ucll.be>
2016-02-22 08:51:07 +0100

    Stakeholder server side

---
+++ src/main/java/be/projecttycoon/rest/admin/StakeholderResourceAdmin.java
@@ -0,0 +1,56 @@
+package be.projecttycoon.rest.admin;
+
+import be.projecttycoon.db.StakeholderRepository;
+import be.projecttycoon.model.Stakeholder;
+import be.projecttycoon.rest.team.StakeholderResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import java.security.Principal;
+
+/**
+ * Created by kiwi on 17/02/2016.
+ */
+@RestController
+@RequestMapping(value = "/api/admin/stakeholders")
+public class StakeholderResourceAdmin extends StakeholderResource{
+
+
+    @Autowired
+    public StakeholderResourceAdmin(StakeholderRepository stakeholderRepository){
+        super(stakeholderRepository);
+    }
+
+    //conflict met showStakeholder(Principal team, @PathVariable long id ) uit StakeholderResource ????
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @Produces("application/json")
+    public Stakeholder showStakeholder(@PathVariable long id ){
+        return stakeholderRepository.findOne(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @Consumes("application/json")
+    public void addStakeholder(Stakeholder stakeholder){
+        stakeholderRepository.save(stakeholder);
+    }
+
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.POST)
+    @Consumes("application/json")
+    public void removeStakeholder(Stakeholder stakeholder){
+        stakeholderRepository.delete(stakeholder);
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    @Consumes("application/json")
+    public void updateStakeholder(Stakeholder stakeholder){
+        stakeholderRepository.save(stakeholder);
+    }
+
+
+}
--- src/main/java/be/projecttycoon/rest/team/StakeholderResource.java
+++ src/main/java/be/projecttycoon/rest/team/StakeholderResource.java
@@ -1,11 +1,16 @@
 package be.projecttycoon.rest.team;
 
 import be.projecttycoon.db.StakeholderRepository;
+import be.projecttycoon.model.Game;
 import be.projecttycoon.model.Stakeholder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.Produces;
+import java.security.Principal;
 import java.util.List;
 
 /**
@@ -14,13 +19,26 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/stakeholders")
 public class StakeholderResource {
-    private final StakeholderRepository stakeholderRepository;
+    protected final StakeholderRepository stakeholderRepository;
 
     @Autowired
     public StakeholderResource(StakeholderRepository stakeholderRepository){
         this.stakeholderRepository = stakeholderRepository;
     }
 
-    public List<Stakeholder> getAllSt
+    @RequestMapping(method = RequestMethod.GET)
+    @Produces("application/json")
+    public List<Stakeholder> getAllStakeHolders(){
+        return this.stakeholderRepository.findAll();
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @Produces("application/json")
+    public Stakeholder showStakeholder(Principal team, @PathVariable long id ){
+        //todo check if team is allowed to see this stakeholder...
+        return stakeholderRepository.findOne(id);
+    }
+
 
 }
