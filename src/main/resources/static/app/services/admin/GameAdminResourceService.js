0d5af5af927846d5522aa6d2d50f2db1d124f0fa
jeroenpinxten <jeroen.pinxten@gmail.com>
2016-02-23 13:28:37 +0100

    Team Antwoorden overzicht

--- src/main/resources/static/app/services/admin/GameAdminResourceService.js
+++ src/main/resources/static/app/services/admin/GameAdminResourceService.js
@@ -17,6 +17,11 @@ angular.module('projecttycoon').factory('GameAdminResource', function($resource)
         postQuestion:{
             method: 'POST',
             url: '/api/admin/games/levelkn'
+        },
+        getTeamsForOverview:{
+            method: 'GET',
+            url: '/api/admin/games/:id/lightteams',
+            isArray: true
         }
     });
 
