ec06d2cb42af11f88e5e4cb4f36348aaad4aeed8
thomas_ge <thomasgeboers@gmail.com>
2016-02-25 16:10:42 +0100

    bugfix : can't post answers to closed level

--- src/main/java/be/projecttycoon/rest/team/KnowledgeAreaScoreResource.java
+++ src/main/java/be/projecttycoon/rest/team/KnowledgeAreaScoreResource.java
@@ -1,11 +1,16 @@
 package be.projecttycoon.rest.team;
 
 import be.projecttycoon.db.KnowledgeAreaScoreRepository;
+import be.projecttycoon.db.TeamRepository;
 import be.projecttycoon.model.KnowledgeAreaScore;
+import be.projecttycoon.model.Team;
+import be.projecttycoon.model.TeamLevelPrestation;
+import be.projecttycoon.rest.exception.NotAuthorizedException;
 import be.projecttycoon.rest.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 /**
@@ -16,10 +21,12 @@ import java.util.List;
 public class KnowledgeAreaScoreResource {
 
     private final KnowledgeAreaScoreRepository knowledgeAreaScoreRepository;
+    private final TeamRepository teamRepository;
 
     @Autowired
-    public KnowledgeAreaScoreResource(KnowledgeAreaScoreRepository knowledgeAreaScoreRepository){
+    public KnowledgeAreaScoreResource(KnowledgeAreaScoreRepository knowledgeAreaScoreRepository, TeamRepository teamRepository){
         this.knowledgeAreaScoreRepository = knowledgeAreaScoreRepository;
+        this.teamRepository = teamRepository;
     }
 
     @RequestMapping(method = RequestMethod.GET)
@@ -39,9 +46,19 @@ public class KnowledgeAreaScoreResource {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public KnowledgeAreaScore updateKnowledgeAreaScore(@PathVariable long id, @RequestBody KnowledgeAreaScore knowledgeAreaScore){
+    public KnowledgeAreaScore updateKnowledgeAreaScore(Principal user, @PathVariable long id, @RequestBody KnowledgeAreaScore knowledgeAreaScore){
         KnowledgeAreaScore knas = getKnowledgeAreaScore(id);
-        knas.setAnswer(knowledgeAreaScore.getAnswer());
-        return knowledgeAreaScoreRepository.save(knas);
+        Team team = teamRepository.findByTeamname(user.getName());
+        TeamLevelPrestation teamLevelPrestation = team.getTeamLevelPrestations().stream()
+                .filter( tlp -> tlp.getKnowledgeAreaScores().contains(knas))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException());
+        if(team.isAdmin() || teamLevelPrestation.getLevel().questionsAreOpen()){
+            knas.setAnswer(knowledgeAreaScore.getAnswer());
+            return knowledgeAreaScoreRepository.save(knas);
+        } else {
+            throw new NotAuthorizedException();
+        }
+
     }
 }
